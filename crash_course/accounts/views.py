cd25439e78b0d8d7265fb8bd182ee8ce42eab3f8
Lone Hilke <lonehilke@gmail.com>
2022-09-26 19:40:18 +0200

    part 5
    part 5. vil ikke vise logo
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- crash_course/accounts/views.py
+++ crash_course/accounts/views.py
@@ -10,3 +10,7 @@ def products(request):
 
 def customer(request):
     return render(request, 'accounts/customer.html')
+
+
+
+# fra: https://www.youtube.com/watch?v=mOu9fpfzyUg&list=PL-51WBLyFTg2vW-_6XBoUpE7vpmoR3ztO&index=5
