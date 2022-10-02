ce08d5c8b3421a0fa479f97cf30f638c9637f718
Lone Hilke <lonehilke@gmail.com>
2022-10-02 15:05:06 +0200

    part 10
    part 10
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- crash_course/accounts/urls.py
+++ crash_course/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk_test>', views.customer, name='customer'),
-    
-]
+    path('create_order/', views.createOrder, name='create_order'),
+    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
