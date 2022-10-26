fe023f46caad8e69f3528b330cd0091f7a7791a0
Lone Hilke <lonehilke@gmail.com>
2022-10-26 19:20:50 +0200

    part 20
    part 20
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- crash_course/crash_course/settings.py
+++ crash_course/crash_course/settings.py
@@ -132,3 +132,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'lonehilke@gmail.com'
+EMAIL_HOST_PASSWORD = '19lone72'
