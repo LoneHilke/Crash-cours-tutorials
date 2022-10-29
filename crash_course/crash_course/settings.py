ec78a99e2a125a4df119bd47772279ba5c517b84
Lone Hilke <lonehilke@gmail.com>
2022-10-29 16:24:06 +0200

    part 21 8:14 inden i video
    part 21
    8:14 inde i video ved migration viser den fejl
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- crash_course/crash_course/settings.py
+++ crash_course/crash_course/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'crash_course.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'user': 'postgres',
+        'PASSWORD': '19lone72',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
